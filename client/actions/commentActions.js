6a1179a8b3b6838996e9c7981955289aa263f5b6
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-30 15:42:33 -0400

    configure actions and reducers

---
+++ client/actions/commentActions.js
@@ -0,0 +1,16 @@
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+export function removeComment(postId, index) {
+  return {
+    type: "REMOVE_COMMENT",
+    index,
+    postId
+  }
+}
--- client/reducers/postReducer.js
+++ client/reducers/postReducer.js
@@ -1,4 +1,5 @@
 function posts(state = [], action) {
+  console.log('The post will change');
   console.log(state, action);
   return state;
 }
