bac410e2aa8f1f8f51afb65120bab1d36f004080
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-30 15:34:13 -0400

    connects the store to the react router

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,6 +4,6 @@ import { routerReducer } from 'react-router-redux';
 import posts from './postReducer';
 import comments from './commentReducer';
 
-const  rootReducer = combineReducers({
-  posts, comments, router: routerReducer
-});
+const rootReducer = combineReducers({posts, comments, routing: routerReducer });
+
+export default rootReducer;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -12,14 +12,18 @@ import Single from './components/Single';
 
 //import react router deps
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store'
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
