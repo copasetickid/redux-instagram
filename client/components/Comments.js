fc88007f4ebedcf4748483fa7bb9b44f048ebd38
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-08-16 01:33:14 -0400

    Displays a single photo and temp space for comments

---
+++ client/components/Comments.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Comments extends React.Component {
+  render() {
+    return(
+      <div className="comment">
+      I'm the comment!!!
+      </div>
+
+    )
+  }
+}
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 class Single extends React.Component {
 
   render() {
+    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[index];
+
     return(
       <div className="single-photo">
-
+        <Photo i={index} post={post} {...this.props} />
+        <Comments/>
       </div>
     )
   }
