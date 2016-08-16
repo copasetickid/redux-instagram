8feb97cef3595f8a843284ab66582a9b5d94b4e3
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-08-16 01:23:17 -0400

    Wire up the posts reducers to increment likes

--- client/reducers/postReducer.js
+++ client/reducers/postReducer.js
@@ -1,7 +1,15 @@
 function posts(state = [], action) {
-  console.log('The post will change');
-  console.log(state, action);
-  return state;
+  switch (action.type){
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0, i), //before the one are are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i+1),
+      ];
+    default:
+      return state;
+  };
 }
 
 export default posts;
