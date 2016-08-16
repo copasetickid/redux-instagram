a451a02c319d6845200c5f403bde4a4839654cfe
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-08-16 02:24:40 -0400

    Enables redux dev tools

--- client/store.js
+++ client/store.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -14,7 +14,11 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+const enchancers = compose(
+   window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+
+const store = createStore(rootReducer, defaultState, enchancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
