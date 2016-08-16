c60b00fad3b963414ab082e3e31c457d1771122b
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-08-16 01:42:53 -0400

    Displays comments for a post

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 
 class Comments extends React.Component {
+  renderComment(comment, index) {
+    return (
+      <div className="comment" key={index}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  }
+
   render() {
     return(
-      <div className="comment">
-      I'm the comment!!!
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden/>
+        </form>
       </div>
 
     )
--- client/components/Single.js
+++ client/components/Single.js
@@ -5,13 +5,16 @@ import Comments from './Comments';
 class Single extends React.Component {
 
   render() {
-    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const { postId } = this.props.params;
+
+    const index = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[index];
+    const postComments = this.props.comments[postId] || [];
 
     return(
       <div className="single-photo">
         <Photo i={index} post={post} {...this.props} />
-        <Comments/>
+        <Comments postComments={postComments}/>
       </div>
     )
   }
